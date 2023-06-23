7dfa07f324750d09fcbfe7f6091551023b8bc914
UI369 <UI369x@gmail.com>
2023-06-23 15:58:23 -0700

    Initial commit

---
+++ contracts/counter.clar
@@ -0,0 +1,33 @@
+
+;; title: counter
+;; version:
+;; summary:
+;; description:
+
+;; traits
+;;
+
+;; token definitions
+;; 
+
+;; constants
+;;
+
+;; data vars
+;;
+
+;; data maps
+;;
+
+;; public functions
+;;
+
+(define-public (hello-world)
+    (ok (print {msg: "Hello World", tip: block-height, sender: tx-sender})))
+
+;; read only functions
+;;
+
+;; private functions
+;;
+
---
+++ tests/counter_test.ts
@@ -0,0 +1,26 @@
+
+import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.7.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
+
+Clarinet.test({
+    name: "Ensure that <...>",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        // arrange: set up the chain, state, and other required elements
+        let wallet_1 = accounts.get("wallet_1")!.address;
+
+        // act: perform actions related to the current test
+        let block = chain.mineBlock([
+            
+            Tx.contractCall("counter", "hello-world", [], wallet_1)
+            
+        ]);
+        console.log(block);
+
+        // assert: review returned data, contract state, and other requirements
+        let inner = block.receipts[0].result.expectOk();
+        let tuple: any = inner.expectTuple();
+        tuple["msg"].expectAscii("Hello World");
+        tuple["tip"].expectUint(1);
+        tuple["sender"].expectPrincipal("ST1SJ3DTE5DN7X54YDH5D64R3BCB6A2AG2ZQ8YPD5");
+    },
+});
